8af404d3aa4967cdf3e5427a3f847d7cec102584
unknown <p.gronotte@web.de>
2014-04-28 21:34:03 +0200

    first struct path added to file

--- getweatherxml.m
+++ getweatherxml.m
@@ -1,4 +1,4 @@
 path = urlwrite('http://api.wunderground.com/api/aad6eaa289129b55/forecast10day/q/Germany/Oldenburg.xml', 'weather.xml');
 xml = xmlread('weather.xml');
 weatherdata= parse_xml(xml);
-%weatherdata.children{1, 1}.children{1, 4}.children{1, 2}.children{1, 1}.children
+data=weatherdata.children{1, 1}.children{1, 4}.children{1, 2}.children{1, 1}.children{1, 1}.children{1, 3}.children{1, 2}.children

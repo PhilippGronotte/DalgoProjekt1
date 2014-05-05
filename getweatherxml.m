7f59d4f3a29a25954d513175c3fb7ac2b8984f03
Caterina Berndt <caterina.berndt@web.de>
2014-05-05 16:20:18 +0200

    Wochentage in String-Matrix

--- getweatherxml.m
+++ getweatherxml.m
@@ -27,11 +27,13 @@ for i=1:10
     data{i,9}=weatherdata.children{1}.children{4}.children{2}.children{1}.children{i}.children{13}.children{2}.children;
     %Humidity
     data{i,10}=weatherdata.children{1}.children{4}.children{2}.children{1}.children{i}.children{18}.children;
-    %struct erstellen; mit struct(i) kann auf die einzelnen Tage
-    %zugegriffen werden, die Tage sind von 1-5 nummeriert
+    
 end
 
 [dateday,datemonth,tmax,tmin,chancerain,avewind,rainfall,snowfall,humidity]=cell2vec(data);
 
-
+%erzeugt eine String-Matrix, um mehrere strings in einer Variable zu
+%speichern; mit weekday(x,1:9) kann auf die einzelnen Wochentage
+%zugegriffen werden, x ist dabei eine Zahl zwischen 1 und 10
+weekday=strvcat([data{21:30}]);
 

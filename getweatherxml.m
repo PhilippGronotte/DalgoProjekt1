4b34ec1744853a24afa91602d93294dc44b87816
unknown <p.gronotte@web.de>
2014-05-05 14:14:43 +0200

    Vollständiges Datenauslesen in Schleife implementiert

--- getweatherxml.m
+++ getweatherxml.m
@@ -2,27 +2,31 @@ path = urlwrite('http://api.wunderground.com/api/aad6eaa289129b55/forecast10day/
 xml = xmlread('weather.xml');
 weatherdata= parse_xml(xml);
 
-%cell-arrays deklarieren
+%cell-array deklarieren
 data={};
 
-%struct deklarieren
-s=struct('day','month','temperature_max','temperature_min');
-
 %Schleife zum Auslesen der minimalen und maximalen Temperatur der nächsten 5 Tage
-for i=1:5
+for i=1:10
     %day
     data{i,1}=weatherdata.children{1}.children{4}.children{2}.children{1}.children{i}.children{1}.children{4}.children;
     %month
-    data{i,2}=weatherdata.children{1}.children{4}.children{2}.children{1}.children{1}.children{1}.children{5}.children;
+    data{i,2}=weatherdata.children{1}.children{4}.children{2}.children{1}.children{i}.children{1}.children{5}.children;
+    %Wochentag
+    data{i,3}=weatherdata.children{1}.children{4}.children{2}.children{1}.children{i}.children{1}.children{15}.children;
     %Tmax
-    data{i,3}=weatherdata.children{1}.children{4}.children{2}.children{1}.children{i}.children{3}.children{2}.children;
+    data{i,4}=weatherdata.children{1}.children{4}.children{2}.children{1}.children{i}.children{3}.children{2}.children;
     %Tmin
-    data{i,4}=weatherdata.children{1}.children{4}.children{2}.children{1}.children{i}.children{4}.children{2}.children;
-   
+    data{i,5}=weatherdata.children{1}.children{4}.children{2}.children{1}.children{i}.children{4}.children{2}.children;
+    %Chance of Rain in percent
+    data{i,6}=weatherdata.children{1}.children{4}.children{2}.children{1}.children{i}.children{9}.children;
+    %Average wind speed
+    data{i,7}=weatherdata.children{1}.children{4}.children{2}.children{1}.children{i}.children{17}.children{2}.children;
+    %Rainfall in mm
+    data{i,8}=weatherdata.children{1}.children{4}.children{2}.children{1}.children{i}.children{10}.children{2}.children;
+    %snow in cm
+    data{i,9}=weatherdata.children{1}.children{4}.children{2}.children{1}.children{i}.children{13}.children{2}.children;
+    %Humidity
+    data{i,10}=weatherdata.children{1}.children{4}.children{2}.children{1}.children{i}.children{18}.children;
     %struct erstellen; mit struct(i) kann auf die einzelnen Tage
     %zugegriffen werden, die Tage sind von 1-5 nummeriert
-    s(i).day=data{i,1};
-    s(i).month=data{i,2};
-    s(i).temperature_max=data{i,3};
-    s(i).temperature_min=data{i,4};
 end

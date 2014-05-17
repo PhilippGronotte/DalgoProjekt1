ca0afc5cb2b59fd39335b626de44bdb7037dc2b3
Caterina Berndt <caterina.berndt@web.de>
2014-05-17 14:06:54 +0200

    icon_switch auf maximale Länge

--- Grafik_einbinden.m
+++ Grafik_einbinden.m
@@ -7,37 +7,39 @@ function [] = Grafik_einbinden(icon,axes_feld,Tag,figure_nr)
 
 %------------Your function implementation here----------------------------- 
 
-icon_switch = char(icon(Tag,1:12));
-
+a=size(icon);
+a=a(2);
+icon_switch = char(icon(Tag,1:a));
+icon_switch(length(icon_switch)+1:14) = ' ';
 %--------------------------------------------------------------------------
 % Wählt Bild in abhängigkeit zur temperatur aus
-if strcmp(icon_switch, 'clear       ')|| strcmp(icon_switch, 'sunny       ')
+if strcmp(icon_switch, 'clear         ')|| strcmp(icon_switch, 'sunny         ')
     Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\sunny.png';
-elseif strcmp(icon_switch, 'partlycloudy') || strcmp(icon_switch, 'partlysunny ')
+elseif strcmp(icon_switch, 'partlycloudy  ') || strcmp(icon_switch, 'partlysunny   ')
    Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\partlycloudy.png';
-elseif strcmp(icon_switch,'flurries    ') || strmcp(icon_switchm , 'chanceflurie')
+elseif strcmp(icon_switch,'flurries      ') || strcmp(icon_switchm , 'chanceflurries')
    Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\flurries.png';
-elseif strcmp(icon_switch, 'chancerain  ')
+elseif strcmp(icon_switch, 'chancerain    ')
    Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\chancerain.png';
-elseif strcmp(icon_switch, 'sleet       ') || strcmp(icon_switch, 'chancesleet ')
+elseif strcmp(icon_switch, 'sleet         ') || strcmp(icon_switch, 'chancesleet   ')
    Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\sleet.png';
-elseif strcmp(icon_switch, 'chancesnow  ')
+elseif strcmp(icon_switch, 'chancesnow    ')
    Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\chancesnow.png';
-elseif strcmp(icon_switch, 'chancetstorm')
+elseif strcmp(icon_switch, 'chancetstorm  ')
    Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\chancetstorms.png';
-elseif strcmp(icon_switch, 'cloudy      ')
+elseif strcmp(icon_switch, 'cloudy        ')
    Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\cloudy.png';
-elseif strcmp(icon_switch, 'fog         ')
+elseif strcmp(icon_switch, 'fog           ')
    Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\fog.png';
-elseif strcmp(icon_switch, 'hazy        ')
+elseif strcmp(icon_switch, 'hazy          ')
    Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\hazy.png';
-elseif strcmp(icon_switch, 'mostlycloudy')
+elseif strcmp(icon_switch, 'mostlycloudy  ')
    Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\mostlycloudy.png';
-elseif strcmp(icon_switch, 'mostlysunny ')
+elseif strcmp(icon_switch, 'mostlysunny   ')
    Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\mostlysunny.png';
-elseif strcmp(icon_switch ,'rain        ')
+elseif strcmp(icon_switch ,'rain          ')
    Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\rain.png';
-elseif strcmp(icon_switch ,'snow        ')
+elseif strcmp(icon_switch ,'snow          ')
    Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\snow.png';
 else 
    Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\NaN.png';

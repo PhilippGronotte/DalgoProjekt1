ff3cd66760bce948e2da7a536118b79f63cb1879
Caterina Berndt <caterina.berndt@web.de>
2014-05-18 13:10:35 +0200

    Bild auslesen aus relativer Pfadangabe

--- Grafik_einbinden.m
+++ Grafik_einbinden.m
@@ -14,35 +14,35 @@ icon_switch(length(icon_switch)+1:14) = ' ';
 %--------------------------------------------------------------------------
 % Wählt Bild in abhängigkeit zur temperatur aus
 if strcmp(icon_switch, 'clear         ')|| strcmp(icon_switch, 'sunny         ')
-    Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\sunny.png';
+    Bild='flat_colorful\png\sunny.png';
 elseif strcmp(icon_switch, 'partlycloudy  ') || strcmp(icon_switch, 'partlysunny   ')
-   Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\partlycloudy.png';
-elseif strcmp(icon_switch,'flurries      ') || strcmp(icon_switchm , 'chanceflurries')
-   Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\flurries.png';
+   Bild='flat_colorful\png\partlycloudy.png';
+elseif strcmp(icon_switch,'flurries      ') || strcmp(icon_switch , 'chanceflurries')
+   Bild='flat_colorful\png\flurries.png';
 elseif strcmp(icon_switch, 'chancerain    ')
-   Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\chancerain.png';
+   Bild='flat_colorful\png\chancerain.png';
 elseif strcmp(icon_switch, 'sleet         ') || strcmp(icon_switch, 'chancesleet   ')
-   Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\sleet.png';
+   Bild='flat_colorful\png\sleet.png';
 elseif strcmp(icon_switch, 'chancesnow    ')
-   Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\chancesnow.png';
+   Bild='flat_colorful\png\chancesnow.png';
 elseif strcmp(icon_switch, 'chancetstorm  ')
-   Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\chancetstorms.png';
+   Bild='flat_colorful\png\chancetstorms.png';
 elseif strcmp(icon_switch, 'cloudy        ')
-   Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\cloudy.png';
+   Bild='flat_colorful\png\cloudy.png';
 elseif strcmp(icon_switch, 'fog           ')
-   Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\fog.png';
+   Bild='flat_colorful\png\fog.png';
 elseif strcmp(icon_switch, 'hazy          ')
-   Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\hazy.png';
+   Bild='flat_colorful\png\hazy.png';
 elseif strcmp(icon_switch, 'mostlycloudy  ')
-   Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\mostlycloudy.png';
+   Bild='flat_colorful\png\mostlycloudy.png';
 elseif strcmp(icon_switch, 'mostlysunny   ')
-   Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\mostlysunny.png';
+   Bild='flat_colorful\png\mostlysunny.png';
 elseif strcmp(icon_switch ,'rain          ')
-   Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\rain.png';
+   Bild='flat_colorful\png\rain.png';
 elseif strcmp(icon_switch ,'snow          ')
-   Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\snow.png';
+   Bild='flat_colorful\png\snow.png';
 else 
-   Bild='C:\Users\User\Desktop\Weather Forecast\flat_colorful\png\NaN.png';
+   Bild='flat_colorful\png\NaN.png';
 
 end
 
